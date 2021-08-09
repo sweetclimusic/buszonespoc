81f22e7a7e5cc6d9ed3b07fb481bd79a89cb5bef
Ashlee Muscroft <ashlee.muscroft@>
2021-08-09 14:54:32 +0100

    Initial Commit

---
+++ PocDFT/PocDFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocDFTApp.swift
+//  PocDFT
+//
+//  Created by ashlee.muscroft on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PocDFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
